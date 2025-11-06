f80c0ce23344dff4e7f0d70f837d3f7db6c24104
ROXANE <AUX-SST@PRESTIGEROSES.COM>
2025-11-06 15:00:05 -0500

    nov06

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/api/registrar', async (req, res) => {
           font-family:sans-serif;
           text-align:center;
           margin-top:80px;
-          background-color:#ffffff;
+          background-color:#b9deff;
           color:white;
         ">
           <h1 style="font-size:72px; color:#f97316; margin-bottom:20px;">
