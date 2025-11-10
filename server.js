72cc9da6b83a09b9b1c8da87478115cffc965fed
ROXANE <AUX-SST@PRESTIGEROSES.COM>
2025-11-10 13:33:00 -0500

    e2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Servidor corriendo ✅</h1>");
@@ -14,5 +14,3 @@ app.get("/api/test-db", (req, res) => {
 app.listen(PORT, () => {
   console.log("🚀 Servidor activo en puerto " + PORT);
 });
-
-
